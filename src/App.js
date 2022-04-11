c2b190ddb4e2c384f1dba43af1f7d7416761818a
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-04-11 09:50:31 +0600

    Routing Done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import ABout from './Components/About/ABout';
 import Header from './Components/Header/Header';
 import Inventory from './Components/Inventory/Inventory';
+import Login from './Components/Login/Login';
 import Orders from './Components/Orders/Orders';
+import Register from './Components/Register/Register';
 import Shop from './Components/Shop/Shop';
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<ABout></ABout>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css'
+
+const Login = () => {
+    return (
+        <>
+         <h1>Hi Mitu</h1>   
+        </>
+    );
+};
+
+export default Login;
